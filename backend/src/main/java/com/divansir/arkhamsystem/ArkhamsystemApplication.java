fdd7096a24cc8e403aab8d99c41710d12f96aab0
Divonsir Scrobut <jscrobut989@gmail.com>
2026-06-09 20:09:03 -0300

    First commit :)

---
+++ backend/src/main/java/com/divansir/arkhamsystem/ArkhamsystemApplication.java
@@ -0,0 +1,13 @@
+package com.divansir.arkhamsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkhamsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkhamsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/divansir/arkhamsystem/ArkhamsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divansir.arkhamsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkhamsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
